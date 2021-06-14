cc694d6eeeb67e7ba869cf35a45a4c8a342b3682
Hakan AYTAYLAN <hakan.aytaylan@gmail.com>
2021-06-14 22:26:21 +0300

    Chapter 4

--- src/index.js
+++ src/index.js
@@ -1,8 +1,58 @@
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
 const app = express();
-
 const port = process.env.PORT || 4000;
+const typeDefs = gql`
+type Note {
+  id: ID!
+  content: String!
+  author: String!
+}
 
-app.get('/', (req, res) => res.send('Hello World!!!'));
+type Query {
+  hello: String!
+  notes: [Note!]!
+  note(id: ID!): Note!
+}
 
-app.listen(port, () => console.log(`Listening on port: ${port}`));
+type Mutation {
+  newNote(content: String!): Note!
+}
+`;
+
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
+];
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id);
+    }
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Adam Scott'
+      };
+      notes.push(noteValue);
+      return noteValue;
+    }
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+  )
+);
