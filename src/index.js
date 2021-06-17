be612b112b71b8f43ec23279fc8654e2b5dedba7
haytaylan <hakan.aytaylan@gmail.com>
2021-06-17 22:49:10 +0300

    Chapter 5

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+require('dotenv').config();
+const db = require('./db');
+const models = require('./models');
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 
+
 const app = express();
 const port = process.env.PORT || 4000;
+const DB_HOST = process.env.DB_HOST;
 const typeDefs = gql`
 type Note {
   id: ID!
@@ -30,23 +35,24 @@ let notes = [
 const resolvers = {
   Query: {
     hello: () => 'Hello world!',
-    notes: () => notes,
-    note: (parent, args) => {
-      return notes.find(note => note.id === args.id);
+    notes: async () => {
+      return await models.Note.find();
+    },
+    note: async (parent, args) => {
+      return await models.Note.findById(args.id);
     }
   },
   Mutation: {
-    newNote: (parent, args) => {
-      let noteValue = {
-        id: String(notes.length + 1),
+    newNote: async (parent, args) => {
+      return await models.Note.create({
         content: args.content,
         author: 'Adam Scott'
-      };
-      notes.push(noteValue);
-      return noteValue;
+      });
     }
   }
 };
+
+db.connect(DB_HOST);
 
 const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app, path: '/api' });
