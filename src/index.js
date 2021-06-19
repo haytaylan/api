20cc56bcf8d8859d09d0347c27f498571669065e
haytaylan <hakan.aytaylan@gmail.com>
2021-06-19 09:42:40 +0300

    Chapter 6

--- src/index.js
+++ src/index.js
@@ -1,30 +1,16 @@
 require('dotenv').config();
 const db = require('./db');
 const models = require('./models');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 
 
 const app = express();
 const port = process.env.PORT || 4000;
 const DB_HOST = process.env.DB_HOST;
-const typeDefs = gql`
-type Note {
-  id: ID!
-  content: String!
-  author: String!
-}
 
-type Query {
-  hello: String!
-  notes: [Note!]!
-  note(id: ID!): Note!
-}
-
-type Mutation {
-  newNote(content: String!): Note!
-}
-`;
 
 let notes = [
   { id: '1', content: 'This is a note', author: 'Adam Scott' },
@@ -32,29 +18,16 @@ let notes = [
   { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
 ];
 
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-    notes: async () => {
-      return await models.Note.find();
-    },
-    note: async (parent, args) => {
-      return await models.Note.findById(args.id);
-    }
-  },
-  Mutation: {
-    newNote: async (parent, args) => {
-      return await models.Note.create({
-        content: args.content,
-        author: 'Adam Scott'
-      });
-    }
-  }
-};
-
 db.connect(DB_HOST);
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: () => {
+    // Add the db models to the context
+    return { models };
+  }
+});
 server.applyMiddleware({ app, path: '/api' });
 
 app.listen({ port }, () =>
